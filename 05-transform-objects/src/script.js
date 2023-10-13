b2e53b1491b6f6de6a68d454b9acd29d5ace3582
Vlad Polevoi <vlad.polewoi1@gmail.com>
2023-10-13 13:58:50 +0300

    feat: :sparkles: init

---
+++ 05-transform-objects/src/script.js
@@ -0,0 +1,57 @@
+import * as THREE from "three"
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl")
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Objects
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+
+// change position
+// mesh.position.x = 0.7
+// mesh.position.y = -0.6
+// mesh.position.z = 1
+
+// same here
+mesh.position.set(0.7, -0.6, 1)
+
+scene.add(mesh)
+
+// Axes helper
+const axesHelper = new THREE.AxesHelper(2)
+scene.add(axesHelper)
+
+// mesh.position.normalize() // normalize the vector to 1
+
+/**
+ * Sizes
+ */
+const sizes = {
+	width: 800,
+	height: 600,
+}
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.set(1, 1, 3)
+scene.add(camera)
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+	canvas: canvas,
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
+
+console.log(mesh.position.length()) // vector length from center of the scene to the mesh
+console.log(mesh.position.distanceTo(camera.position))

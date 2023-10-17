a53031ef76c35091def817c0d429a4d9b39238da
Vlad Polevoi <vlad.polewoi1@gmail.com>
2023-10-17 18:09:46 +0300

    feat: :sparkles: 13 - 3D Text

--- 13-3d-text/src/script.js
+++ 13-3d-text/src/script.js
@@ -1,6 +1,10 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'lil-gui'
+import * as THREE from "three"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
+import * as dat from "lil-gui"
+
+// import fontLoader
+import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js"
+import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry.js"
 
 THREE.ColorManagement.enabled = false
 
@@ -11,54 +15,122 @@ THREE.ColorManagement.enabled = false
 const gui = new dat.GUI()
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl")
 
 // Scene
 const scene = new THREE.Scene()
+
+// TASK 2: Add axes helper
+const axesHelper = new THREE.AxesHelper()
+scene.add(axesHelper)
 
 /**
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const matcapTexture = textureLoader.load("/textures/matcaps/8.png")
+
+// Fonts
+// TASK 1: Load fonts
+const fontLoader = new FontLoader()
+fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
+	const textGeometry = new TextGeometry("Hello Three.js", {
+		font,
+		size: 0.5,
+		height: 0.2,
+		curveSegments: 4,
+		bevelEnabled: true,
+		bevelThickness: 0.03,
+		bevelSize: 0.02,
+		bevelOffset: 0,
+		bevelSegments: 5,
+	})
+	// TASK 3: Frustum culling
+	// textGeometry.computeBoundingBox()
+	// textGeometry.translate(
+	// 	(textGeometry.boundingBox.max.x - 0.02) * -0.5,
+	// 	(textGeometry.boundingBox.max.y - 0.02) * -0.5,
+	// 	(textGeometry.boundingBox.max.z - 0.03) * -0.5
+	// )
+	// // to check if it centered correctly
+	// // min and max should be module equals
+	// textGeometry.computeBoundingBox()
+	// console.log(textGeometry.boundingBox)
+	// TASK 4: center easy way
+	textGeometry.center()
+
+	const material = new THREE.MeshMatcapMaterial()
+	material.matcap = matcapTexture
+	// textMaterial.wireframe = true
+	const text = new THREE.Mesh(textGeometry, material)
+
+	scene.add(text)
+
+	// TASK 5: add donuts
+
+	console.time("donut")
+	const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45)
+
+	for (let i = 0; i < 100; i++) {
+		const donut = new THREE.Mesh(donutGeometry, material)
+
+		donut.position.x = (Math.random() - 0.5) * 10
+		donut.position.y = (Math.random() - 0.5) * 10
+		donut.position.z = (Math.random() - 0.5) * 10
+
+		donut.rotation.x = Math.random() * Math.PI
+		donut.rotation.y = Math.random() * Math.PI
+
+		const scale = Math.random()
+		donut.scale.set(scale, scale, scale)
+
+		scene.add(donut)
+	}
+	console.timeEnd("donut")
+})
 
 /**
  * Object
  */
 const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
+	new THREE.BoxGeometry(1, 1, 1),
+	new THREE.MeshBasicMaterial()
 )
 
-scene.add(cube)
+// scene.add(cube)
 
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+	width: window.innerWidth,
+	height: window.innerHeight,
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener("resize", () => {
+	// Update sizes
+	sizes.width = window.innerWidth
+	sizes.height = window.innerHeight
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+	// Update camera
+	camera.aspect = sizes.width / sizes.height
+	camera.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+	// Update renderer
+	renderer.setSize(sizes.width, sizes.height)
+	renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+const camera = new THREE.PerspectiveCamera(
+	75,
+	sizes.width / sizes.height,
+	0.1,
+	100
+)
 camera.position.x = 1
 camera.position.y = 1
 camera.position.z = 2
@@ -72,7 +144,7 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+	canvas: canvas,
 })
 renderer.outputColorSpace = THREE.LinearSRGBColorSpace
 renderer.setSize(sizes.width, sizes.height)
@@ -83,18 +155,17 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+const tick = () => {
+	const elapsedTime = clock.getElapsedTime()
 
-    // Update controls
-    controls.update()
+	// Update controls
+	controls.update()
 
-    // Render
-    renderer.render(scene, camera)
+	// Render
+	renderer.render(scene, camera)
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+	// Call tick again on the next frame
+	window.requestAnimationFrame(tick)
 }
 
 tick()
